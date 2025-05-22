7252ffe487e64d733e863faf23d7a213242042a7
melpscosta <melpscosta@gmail.com>
2025-05-22 13:10:26 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Mottu.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("OracleDb")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
